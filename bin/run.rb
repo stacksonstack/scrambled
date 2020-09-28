d491f4f214feb372123700179fcb9fc6986456c5
billyott <willrichott@gmail.com>
2020-09-28 15:36:44 -0400

    testing api.

--- bin/run.rb
+++ bin/run.rb
@@ -2,8 +2,22 @@ require 'pry'
 require 'tty-prompt'
 require_relative '../config/environment'
 
-puts "HELLO WORLD"
+
+word = RandomWord.nouns.next
+new_word = "didn't change"
+if word.include?("_")
+    new_word = word.split("_")[0]
+end
+
+
+url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+
+test_response = RestClient.get(url)
+test_data = JSON.parse(test_response)
+
+
+binding.pry
 
 cli = CLI.new
 
-binding.pry
+
