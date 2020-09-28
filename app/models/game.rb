0137098647fe59eee4dde085c19d4fa469228f28
billyott <willrichott@gmail.com>
2020-09-28 18:58:03 -0400

    updated all models.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,21 @@
 class Game < ActiveRecord::Base
-    attr_reader :player_id, :word_id
+    # attr_reader :player_id, :word_id
     belongs_to :word 
     belongs_to :player 
 
     # attr_accessor :status, :incorrect_guesses
-    def start_game
+    def self.start_game(player,word)
+        Game.create(player: player, word: word, status: "in progress", incorrect_guesses: 0)
     end
+
+    def game_lost
+        self.status = "lost"
+    end
+
+    def game_won
+        self.status = "won"
+    end
+
 
 end
 
@@ -15,3 +25,13 @@ end
 # #game_over
 # Class methods
 # .game_start
+
+
+
+# -game starts (Game.start_game)
+# -word displayed, player enters first guess
+# -check if guess matches "word"
+# -if word doesnt match, 
+#     +1 to incorrect guesses && check if guesses == 3 (if so, call game_lost)
+#     get another guess from player & repeat
+# -if word does match, call game_won
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,15 @@
 class Word < ActiveRecord::Base
-    attr_reader :word_type, :definition, :word, :difficulty
+    # attr_reader :word_type, :definition, :word, :difficulty
     has_many :games
     has_many :players , through: :games 
+
+    def hint
+        if word.difficulty == "Easy"
+            self.word_type
+        else
+            self.definition
+        end
+    end
     
 end
 
